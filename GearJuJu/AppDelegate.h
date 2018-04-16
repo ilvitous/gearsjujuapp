77151d384d0235f61d222a1e4056f6475a486fd1
ilvitous <andreav@pridegroup.us>
2018-04-16 11:30:17 -0700

    Initial Commit

---
+++ GearJuJu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GearJuJu
+//
+//  Created by Andrea Vitale on 4/16/18.
+//  Copyright © 2018 Pride Group LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GearJuJu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GearJuJu
+//
+//  Created by Andrea Vitale on 4/16/18.
+//  Copyright © 2018 Pride Group LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
